00186275dded6c938ba5c4d03301d85017ab9c46
Philip Ellis <philip.ellis@sailpoint.com>
2024-03-19 11:47:08 -0400

    resolved issues with initial loading

--- src/routes/home/transform-editor/+server.ts
+++ src/routes/home/transform-editor/+server.ts
@@ -6,7 +6,9 @@ import {
 } from 'sailpoint-api-client';
 
 export const GET: RequestHandler = async ({ locals }) => {
+	console.log('starting get')
     const config = createConfiguration(locals.session!.baseUrl, locals.idnSession!.access_token);
+	console.log('got config')
 	const api = new TransformsApi(config);
 
 	const transformResp = await Paginator.paginate(api, api.listTransforms, { limit: 1000 });
