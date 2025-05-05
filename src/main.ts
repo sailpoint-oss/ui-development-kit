884a978f70a93bcf6a333bd2d0b71c34648468c7
Philip Ellis <philip.ellis@sailpoint.com>
2025-05-05 13:11:42 -0400

    fixed linting errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
  // adjust if your routes file is elsewhere
